218896ac752486d59d0ed7a1c22e86e0c0606a9f
BlackbirdSQL <120905720+BlackbirdSQL@users.noreply.github.com>
2023-05-13 14:11:20 +0200

    v9.0.72-beta SE icon cleanup

--- BlackbirdDsl/Include/AssemblyInfo.h
+++ BlackbirdDsl/Include/AssemblyInfo.h
@@ -2,9 +2,9 @@
 
 #define Assembly_Copyright L"Copyright © BlackbirdSql 2023"
 #define Assembly_Description L"BlackbirdSql DSL Parser"
-#define File_Version 9,1,0,71
-#define File_Version_Str "9.1.0.71"
+#define File_Version 9,1,0,72
+#define File_Version_Str "9.1.0.72"
 
 //CLR assembly version
-#define Assembly_Version L"9.1.0.71"
+#define Assembly_Version L"9.1.0.72"
 
