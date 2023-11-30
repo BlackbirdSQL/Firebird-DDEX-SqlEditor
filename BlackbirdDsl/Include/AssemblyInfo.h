7d1d756de0403ebc9d38700d6f2856e1dbd5d4ba
BlackbirdSQL <120905720+BlackbirdSQL@users.noreply.github.com>
2023-12-01 00:33:50 +0200

    Version sync.

--- BlackbirdDsl/Include/AssemblyInfo.h
+++ BlackbirdDsl/Include/AssemblyInfo.h
@@ -2,9 +2,9 @@
 
 #define Assembly_Copyright L"Copyright © BlackbirdSql 2023"
 #define Assembly_Description L"BlackbirdSql DSL Parser"
-#define File_Version 10,0,0,0
-#define File_Version_Str "10.0.0.0"
+#define File_Version 10,0,0,101
+#define File_Version_Str "10.0.0.101"
 
 //CLR assembly version
-#define Assembly_Version L"10.0.0.0"
+#define Assembly_Version L"10.0.0.101"
 
